b5325f3e1e507e6848532e64d6ccdd9146b45c72
snaku <48354721+snaku@users.noreply.github.com>
2025-05-16 16:21:42 +0200

    Add files via upload

---
+++ include/MM_Player.h
@@ -0,0 +1,13 @@
+#ifndef MM_PLAYER_H
+#define MM_PLAYER_H
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+struct SavePlayerData;
+
+bool ModifyHealth(int unused_1, short heart);
+void ModifyMagic();
+
+#endif
