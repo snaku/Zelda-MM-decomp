0a570c3cf2983d1140015de129a34d065f689d72
snaku <48354721+snaku@users.noreply.github.com>
2025-05-16 16:22:43 +0200

    Update MM_Save.c
    Moved modifyHealth to MM_Player.c

--- src/MM_Save.c
+++ src/MM_Save.c
@@ -1,29 +1,2 @@
 #include "../include/MM_Save.h"
 
-// FUN_80115908
-bool ModifyHealth(int unused_1, short healthChange)
-{
-    if (healthChange < 0 && gPlayerData.doubleDefense)
-    {
-        healthChange >>= 1; // division par 2
-    }
-    else if (healthChange > 0)
-    {
-        // FUN_8019f0c8(0x480b); // playSound ?
-    }
-
-    gPlayerData.health += healthChange;
-
-    if (gPlayerData.healthCapacity < gPlayerData.health)
-    {
-        gPlayerData.health = gPlayerData.healthCapacity;
-    }
-    
-    if (gPlayerData.health <= 0)
-    {
-        gPlayerData.health = 0;
-        return false; // mort
-    }
-    
-    return true; // vivant
-}
