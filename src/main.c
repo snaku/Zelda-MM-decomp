22efcef3e61482495b9f29117fe6906bb42eff9a
snaku <48354721+snaku@users.noreply.github.com>
2025-05-16 16:25:54 +0200

    Add files via upload

---
+++ src/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "../include/MM_Actor.h"
+#include "../include/MM_Save.h"
+#include "../include/MM_Item.h"
+#include "../include/MM_Player.h"
+
+int main()
+{
+    // test
+    ModifyHealth(0, 5);
+    ModifyMagic();
+
+    return 0;
+}
